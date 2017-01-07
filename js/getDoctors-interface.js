090d119d869c40435d2a16a07ea30491b56c5045
Guest User <Guest@epicodus-81.local>
2017-01-06 16:17:21 -0800

    added google maps api and separated front and back-end javascript

--- js/getDoctors-interface.js
+++ js/getDoctors-interface.js
@@ -1,4 +1,27 @@
 var getDoctors = require('./../js/getDoctors.js').getDoctors;
+var googleApiKey = require('./../.env').googleApiKey;
+
+var displayDoctors = function(doctors) {
+  doctors.forEach(function(doctor) {
+    $('#doctorList').append("<h5>" + doctor.name + "</h5> (" + doctor.distance + " miles away)<br>" + doctor.bio + "<br><br><br><div id='map"+doctors.indexOf(doctor)+"'><div><br><br><br><br>");
+
+    function initMap() {
+      $.ajax({
+        url: 'https://maps.googleapis.com/maps/api/js?key=' + googleApiKey,
+        dataType: "script"
+      }).done(function() {
+        docLoc = eval('({' + doctor.location + '})');
+        var map = new google.maps.Map(document.getElementById("map".concat(doctors.indexOf(doctor))), {
+          zoom: 15,
+          center: docLoc,
+          mapTypeId : google.maps.MapTypeId.ROADMAP
+        });
+      });
+    }
+
+    initMap();
+  });
+};
 
 $(document).ready(function() {
   $("#submit").click(function() {
@@ -9,6 +32,6 @@ $(document).ready(function() {
       this.distance = distance;
       this.location = location;
     }
-    getDoctors(ailmentInput);
+    getDoctors(ailmentInput, displayDoctors);
   });
 });
--- js/getDoctors.js
+++ js/getDoctors.js
@@ -1,11 +1,8 @@
 var apiKey = require('./../.env').apiKey;
 
-
-
-exports.getDoctors = function(medicalIssue) {
+exports.getDoctors = function(medicalIssue, displayFunction) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
-      console.log(result);
       doctors = [];
       function Doctor(name, bio, distance, location) {
         this.name = name;
@@ -13,19 +10,11 @@ exports.getDoctors = function(medicalIssue) {
         this.distance = distance;
         this.location = location;
       }
-      arrayLength = result.data.length;
-      for (i = 0; i < arrayLength; i++) {
-      var new_doctor = new Doctor(result.data[i].practices[0].name, result.data[i].profile.bio, result.data[i].practices[0].distance.toFixed(2), "lat: " + result.data[i].practices[0].lat + ", lon: " + result.data[i].practices[0].lon);
+      for (i = 0; i < result.data.length; i++) {
+      var new_doctor = new Doctor(result.data[i].practices[0].name, result.data[i].profile.bio, result.data[i].practices[0].distance.toFixed(2), "lat: " + result.data[i].practices[0].lat + ", lng: " + result.data[i].practices[0].lon);
       doctors.push(new_doctor);
       }
-      doctors.forEach(function(doctor) {
-        $('#doctorList').append(doctor.name);
-        $('#doctorList').append("<br>");
-        $('#doctorList').append(doctor.bio);
-        $('#doctorList').append("<br>");
-        $('#doctorList').append(doctor.distance);
-        $('#doctorList').append(" miles away<br><br><br>");
-      });
+      displayFunction(doctors);
     })
    .fail(function(error){
       console.log("fail");
